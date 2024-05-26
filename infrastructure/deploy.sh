8442ebb43d26ddaf3eb9377cbaafd19361167ce0
Kiryl Shytsikau <shytsikau.kiryl@gmail.com>
2024-05-26 23:08:49 +0300

    SET - Module 3 - CI/CD in Cloud - add auto-approve to the deploy.sh script

--- infrastructure/deploy.sh
+++ infrastructure/deploy.sh
@@ -3,15 +3,15 @@
 if [ "$1" == "dev" ]; then
   terraform workspace select dev || terraform workspace new dev
   terraform init
-  terraform apply -var-file='tf-dev/terraform.tfvars'
+  terraform apply -auto-approve -var-file='tf-dev/terraform.tfvars'
 elif [ "$1" == "qa" ]; then
   terraform workspace select qa || terraform workspace new qa
   terraform init
-  terraform apply -var-file='tf-qa/terraform.tfvars'
+  terraform apply -auto-approve -var-file='tf-qa/terraform.tfvars'
 elif [ "$1" == "prod" ]; then
   terraform workspace select prod || terraform workspace new prod
   terraform init
-  terraform apply -var-file='tf-prod/terraform.tfvars'
+  terraform apply -auto-approve -var-file='tf-prod/terraform.tfvars'
 else
   echo "Usage: $0 {dev|qa|prod}"
   exit 1
