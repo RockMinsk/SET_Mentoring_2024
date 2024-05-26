e221108452c595187c21a6ebda80a244624ee2b9
Kiryl Shytsikau <shytsikau.kiryl@gmail.com>
2024-05-27 01:32:37 +0300

    SET - Module 3 - CI/CD in Cloud - DEBUG terraform

--- infrastructure/modules/tf-environment/main.tf
+++ infrastructure/modules/tf-environment/main.tf
@@ -74,47 +74,47 @@ data "azurerm_storage_account_blob_container_sas" "storage-container-sas" {
   }
 }
 
-#resource "azurerm_cosmosdb_account" "cosmosdb" {
-#  name                = "${var.prefix}-cosmosdb-${var.timestamp}"
-#  location            = azurerm_resource_group.rg.location
-#  resource_group_name = azurerm_resource_group.rg.name
-#  offer_type          = "Standard"
-#  kind                = "GlobalDocumentDB"
-#  # NOTE. Only one free account (F0) available for subscription
-#  free_tier_enabled   = true
-#
-#  consistency_policy {
-#    consistency_level       = "BoundedStaleness"
-#    max_interval_in_seconds = 5
-#    max_staleness_prefix    = 100
-#  }
-#
-#  geo_location {
-#    location          = azurerm_resource_group.rg.location
-#    failover_priority = 0
-#  }
-#
-#  timeouts {
-#    create = "15m"
-#    delete = "15m"
-#  }
-#}
-#
-#resource "azurerm_cosmosdb_sql_database" "cosmosdb-database" {
-#  name                = "${var.prefix}-cosmosdb-database-${var.timestamp}"
-#  resource_group_name = azurerm_resource_group.rg.name
-#  account_name        = azurerm_cosmosdb_account.cosmosdb.name
-#  throughput          = 400
-#}
-#
-#resource "azurerm_cosmosdb_sql_container" "cosmosdb-container" {
-#  name                = "${var.prefix}-cosmosdb-container-${var.timestamp}"
-#  resource_group_name = azurerm_resource_group.rg.name
-#  account_name        = azurerm_cosmosdb_account.cosmosdb.name
-#  database_name       = azurerm_cosmosdb_sql_database.cosmosdb-database.name
-#  partition_key_path  = "/id"
-#  throughput          = 400
-#}
+resource "azurerm_cosmosdb_account" "cosmosdb" {
+  name                = "${var.prefix}-cosmosdb-${var.timestamp}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+  # NOTE. Only one free account (F0) available for subscription
+  free_tier_enabled   = true
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 5
+    max_staleness_prefix    = 100
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+
+  timeouts {
+    create = "15m"
+    delete = "15m"
+  }
+}
+
+resource "azurerm_cosmosdb_sql_database" "cosmosdb-database" {
+  name                = "${var.prefix}-cosmosdb-database-${var.timestamp}"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.cosmosdb.name
+  throughput          = 400
+}
+
+resource "azurerm_cosmosdb_sql_container" "cosmosdb-container" {
+  name                = "${var.prefix}-cosmosdb-container-${var.timestamp}"
+  resource_group_name = azurerm_resource_group.rg.name
+  account_name        = azurerm_cosmosdb_account.cosmosdb.name
+  database_name       = azurerm_cosmosdb_sql_database.cosmosdb-database.name
+  partition_key_path  = "/id"
+  throughput          = 400
+}
 
 resource "azurerm_servicebus_namespace" "sb-namespace" {
   name                = "${var.prefix}-sb-namespace-${var.timestamp}"
